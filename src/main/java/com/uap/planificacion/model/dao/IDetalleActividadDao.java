e4ea7dbe6c058a505315a63db85bc7b7ee0992fa
marcocuellar7293@gmail.com <marcocuellar7293@gmail.com>
2024-07-02 08:28:15 -0400

    sdfsdf

--- src/main/java/com/uap/planificacion/model/dao/IDetalleActividadDao.java
+++ src/main/java/com/uap/planificacion/model/dao/IDetalleActividadDao.java
@@ -32,7 +32,7 @@ public interface IDetalleActividadDao extends CrudRepository<DetalleActividad, L
                 "LEFT JOIN lugar_subdetalleactividad ls ON ls.id_sub_detalle_actividad = sda.id_sub_detalle_actividad\n" + //
                 "LEFT JOIN lugar l ON l.id_lugar = ls.id_lugar\n" + //
                 "WHERE da.estado != 'X'\n" + //
-                "AND da.fecha_detalle_actividad BETWEEN ?1 AND ?2", nativeQuery = true)
+                "AND da.fecha_detalle_actividad BETWEEN ?1 AND ?2 ORDER BY da.fecha_detalle_actividad ASC", nativeQuery = true)
     public List<Object[]> reporteGeneral(Date fecha_inicio, Date fecha_final);
 
     @Query(value = "select a.descripcion_actividad, ta.nombre_tipo_actividad, uf.nom_unidad, l.nombre_lugar, da.fecha_detalle_actividad, sda.hora_inicio, sda.hora_final, da.costo " +
